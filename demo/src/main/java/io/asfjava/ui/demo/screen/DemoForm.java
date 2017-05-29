59da3bf0a755cb0952a011e67664367c7989f5ac
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-05-29 15:01:49 +0100

    textarea example #3

--- demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
+++ demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
@@ -1,23 +1,24 @@
 package io.asfjava.ui.demo.screen;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import io.asfjava.ui.core.form.ComboBox;
+import io.asfjava.ui.core.form.TextArea;
 import io.asfjava.ui.core.form.TextField;
 
 public class DemoForm implements Serializable {
 
-	@TextField(title = "First Name",placeHolder = "Your first name")
+	@TextField(title = "First Name", placeHolder = "Your first name", description="This is a description for your first name field")
 	private String firstName;
 
-	@TextField(title = "Last Name",placeHolder = "Your last name")
+	@TextField(title = "Last Name", placeHolder = "Your last name")
 	private String lastName;
-	
-	@ComboBox(title="Gender",values={"Male","Female"})
+
+	@ComboBox(title = "Gender", values = { "Male", "Female" })
 	private String gender;
 
-	private Date birthDate;
+	@TextArea(title = "Address", placeHolder = "Fill your address please", description = "This is textarea")
+	private String address;
 
 	public String getFirstName() {
 		return firstName;
@@ -35,16 +36,16 @@ public class DemoForm implements Serializable {
 		this.lastName = lastName;
 	}
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
-	
 	public String getGender() {
 		return gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 	private static final long serialVersionUID = -5073515619469444978L;
