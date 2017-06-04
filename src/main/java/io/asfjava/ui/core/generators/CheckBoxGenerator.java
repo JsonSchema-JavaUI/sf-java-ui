6ca52959c2dcc314c492e904408591eb248684b8
Fakhreddine Belgaied <Fakhreddine10@users.noreply.github.com>
2017-06-04 04:18:52 +0100

    Create CheckBoxGenerator.java
    CheckBoxGenerator annotation

---
+++ src/main/java/io/asfjava/ui/core/generators/CheckBoxGenerator.java
@@ -0,0 +1,62 @@
+package io.asfjava.ui.core.generators;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.asfjava.ui.core.form.CheckBox;
+import io.asfjava.ui.core.form.TitleMapsAdapter;
+
+public class CheckBoxGenerator implements FormDefinitionGenerator {
+
+	@Override
+	public void generate(ObjectNode fieldFormDefinition, Field field) {
+		CheckBox annotation = field.getAnnotation(CheckBox.class);
+		fieldFormDefinition.put("key", field.getName());
+		fieldFormDefinition.put("type", "checkboxes");
+		fieldFormDefinition.put("multiple", annotation.multiple());
+		fieldFormDefinition.put("required", annotation.required());
+		ObjectMapper checkBoxMapper = new ObjectMapper();
+		ArrayNode titlesMap = checkBoxMapper.createArrayNode();
+		String[] checkBoxValues = annotation.values();
+		if (checkBoxValues.length != 0) {
+			for (String value : checkBoxValues) {
+				ObjectNode entry = checkBoxMapper.createObjectNode();
+				if (value.equals(value.toUpperCase())) {
+					entry.put("name", value.toLowerCase());
+				} else if (value.equals(value.toLowerCase())) {
+					entry.put("name", value.replace(value.substring(0, 1), value.substring(0, 1).toUpperCase()));
+				} else {
+					entry.put("name", value);
+				}
+				entry.put("value", value);
+				titlesMap.add(entry);
+			}
+			fieldFormDefinition.set("titleMap", titlesMap);
+		} else if (!annotation.titleMap().equals(TitleMapsAdapter.class)) {
+
+			try {
+				Map<String, Object> map = (annotation.titleMap()).newInstance().getPossibleValues();
+
+				for (Map.Entry<String, Object> iterator : map.entrySet()) {
+					ObjectNode entry = checkBoxMapper.createObjectNode();
+					entry.put("name", iterator.getKey());
+					entry.putPOJO("value", iterator.getValue());
+					titlesMap.add(entry);
+				}
+				fieldFormDefinition.set("titleMap", titlesMap);
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+
+	@Override
+	public String getAnnoation() {
+		return CheckBox.class.getName();
+	}
+
+}
