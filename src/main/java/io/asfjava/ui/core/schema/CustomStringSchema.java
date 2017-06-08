8d89a5ca8498b2fc3ef8bd6aa8a1acefdc8ebe6c
fakhreddine <fakhreddine@fakhreddine-S551LB>
2017-06-08 17:40:41 +0100

    Create Schema decorator in order to edit the jsonSchema with a flexible way

--- src/main/java/io/asfjava/ui/core/schema/CustomStringSchema.java
+++ src/main/java/io/asfjava/ui/core/schema/CustomStringSchema.java
@@ -3,6 +3,12 @@ package io.asfjava.ui.core.schema;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.module.jsonSchema.types.StringSchema;
 
+import io.asfjava.ui.core.SchemaDecoratorFactory;
+import io.asfjava.ui.core.form.CheckBox;
+import io.asfjava.ui.core.form.ComboBox;
+import io.asfjava.ui.core.form.Password;
+import io.asfjava.ui.core.form.RadioBox;
+import io.asfjava.ui.core.form.TextArea;
 import io.asfjava.ui.core.form.TextField;
 
 class CustomStringSchema extends StringSchema {
@@ -10,9 +16,29 @@ class CustomStringSchema extends StringSchema {
 	@Override
 	public void enrichWithBeanProperty(BeanProperty beanProperty) {
 		super.enrichWithBeanProperty(beanProperty);
-		TextField annotation = beanProperty.getAnnotation(TextField.class);
-		if (annotation != null && !annotation.pattern().isEmpty()) {
-			this.setPattern(annotation.pattern());
+		if (beanProperty.getAnnotation(TextField.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(TextField.class.getName()).customizeSchema(beanProperty,
+					this);
+		}
+		if (beanProperty.getAnnotation(ComboBox.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(ComboBox.class.getName()).customizeSchema(beanProperty,
+					this);
+		}
+		if (beanProperty.getAnnotation(CheckBox.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(CheckBox.class.getName()).customizeSchema(beanProperty,
+					this);
+		}
+		if (beanProperty.getAnnotation(RadioBox.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(RadioBox.class.getName()).customizeSchema(beanProperty,
+					this);
+		}
+		if (beanProperty.getAnnotation(TextArea.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(TextArea.class.getName()).customizeSchema(beanProperty,
+					this);
+		}
+		if (beanProperty.getAnnotation(Password.class) != null) {
+			SchemaDecoratorFactory.getInstance().getGenerator(TextField.class.getName()).customizeSchema(beanProperty,
+					this);
 		}
 	}
 
