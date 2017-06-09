8dd2bb4eed9a5fcf484881315b6ed69696330830
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-06-10 00:35:23 +0100

    :art: use Java 8 features
    iterate using for earch
    use lambda expression (consumer)
    use ifPresent to avoid nullpointer exception (java.util.Optional)

--- src/main/java/io/asfjava/ui/core/schema/CustomStringSchema.java
+++ src/main/java/io/asfjava/ui/core/schema/CustomStringSchema.java
@@ -13,8 +13,9 @@ class CustomStringSchema extends StringSchema {
 	public void enrichWithBeanProperty(BeanProperty beanProperty) {
 		super.enrichWithBeanProperty(beanProperty);
 		Iterable<Annotation> it = beanProperty.getMember().annotations();
-		String anno = it.iterator().next().annotationType().getName();
-		SchemaDecoratorFactory.getInstance().getDecorator(anno).customizeSchema(beanProperty, this);
+		it.forEach(
+				annotation -> SchemaDecoratorFactory.getInstance().getDecorator(annotation.annotationType().getName())
+						.ifPresent(decorator -> decorator.customizeSchema(beanProperty, this)));
 	}
 
 }
