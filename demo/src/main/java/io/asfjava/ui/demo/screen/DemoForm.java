e5426459adcb4e35d8f9e9fa77458845680735a9
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-06-13 12:43:24 +0100

    add new @index annotation
    handle fields order fix #51

--- demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
+++ demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
@@ -2,85 +2,83 @@ package io.asfjava.ui.demo.screen;
 
 import java.io.Serializable;
 
-import io.asfjava.ui.core.form.CheckBox;
-import io.asfjava.ui.core.form.ComboBox;
-import io.asfjava.ui.core.form.Number;
+import io.asfjava.ui.core.form.Index;
 import io.asfjava.ui.core.form.Password;
-import io.asfjava.ui.core.form.RadioBox;
-import io.asfjava.ui.core.form.Tab;
-import io.asfjava.ui.core.form.TextArea;
 import io.asfjava.ui.core.form.TextField;
 
 public class DemoForm implements Serializable {
 
-	
-	@TextField(title = "Pesonal Website",fieldAddonLeft="http://", description = "This is TextField with fieldAddonLeft")
+	@Index(3)
+	@TextField(title = "Personal Website")
 	private String webSite;
-	
-	@TextField(title = "Your Github Mail",fieldAddonRight="@github.com", description = "This is TextField with fieldAddonRight")
+
+	@Index(2)
+	@TextField(title = "Your Github Mail")
 	private String gitHub;
-	
-//	@Tab(title = "Contact", index = 2)
-	@Password(title = "Password", placeHolder = "Please set you password", description = "This is password")
+
+	@Index(4)
+	@Password(title = "Password")
 	private String password;
 
-	@Tab(title = "Info", index = 1)
-	@TextField(title = "First Name", placeHolder = "Your first name", description = "This is a description for your first name field")
+	@Index(0)
+	@TextField(title = "First Name")
 	private String firstName;
 
-//	@Tab(title = "Info", index = 1)
-	@TextField(title = "Last Name", placeHolder = "Your last name")
-	private String lastName;
+	// @Tab(title = "Info", index = 1)
+	// @TextField(title = "Last Name", placeHolder = "Your last name")
+	// private String lastName;
+	//
+	// @Tab(title = "Contact", index = 2)
+	// @TextField(title = "eMail", placeHolder = "Your email", pattern =
+	// "^\\S+@\\S+$", validationMessage = "Your mail must be in this format
+	// jhondoe@example.com", description = "This is Text Field with pattern and
+	// validation message")
+	// private String email;
+	//
+	// @Tab(title = "Additional Info", index = 3)
+	// @Number(title = "Number of children", placeHolder = "Number of children",
+	// description = "This is a number")
+	// private Integer number;
+	//
+	//
+	// @Tab(title = "Info", index = 1)
+	// @ComboBox(title = "Gender", titleMap = GenderTitleMap.class)
+	// private String gender;
+	//
+	//// @Tab(title = "Additional Info", index = 3)
+	// @ComboBox(title = "Currency", values = { "euro", "dollar" })
+	// private String currency;
+	//
+	// @Tab(title = "Additional Info", index = 3)
+	// @RadioBox(title = "Civil State", titleMap = CivilStateTitelsMap.class)
+	// private String civilState;
+	//
+	//// @Tab(title = "Contact", index = 2)
+	// @TextArea(title = "Address", placeHolder = "Fill your address please",
+	// description = "This is textarea")
+	// private String address;
 
-	@Tab(title = "Contact", index = 2)
-	@TextField(title = "eMail", placeHolder = "Your email", pattern = "^\\S+@\\S+$", validationMessage = "Your mail must be in this format jhondoe@example.com", description = "This is Text Field with pattern and validation message")
-	private String email;
-
-	@Tab(title = "Additional Info", index = 3)
-	@Number(title = "Number of children", placeHolder = "Number of children", description = "This is a number")
-	private Integer number;
-
-
-	@Tab(title = "Info", index = 1)
-	@ComboBox(title = "Gender", titleMap = GenderTitleMap.class)
-	private String gender;
-
-//	@Tab(title = "Additional Info", index = 3)
-	@ComboBox(title = "Currency", values = { "euro", "dollar" })
-	private String currency;
-
-	@Tab(title = "Additional Info", index = 3)
-	@RadioBox(title = "Civil State", titleMap = CivilStateTitelsMap.class)
-	private String civilState;
-
-//	@Tab(title = "Contact", index = 2)
-	@TextArea(title = "Address", placeHolder = "Fill your address please", description = "This is textarea")
-	private String address;
-
-	@Tab(title = "Additional Info", index = 3)
-	@CheckBox(title = "Color", values = { "red", "bleu", "green" }, defaultvalue = "red")
-	private String color;
-
-	public String getFirstName() {
-		return firstName;
-	}
+	// @Tab(title = "Additional Info", index = 3)
+	// @CheckBox(title = "Color", values = { "red", "bleu", "green" },
+	// defaultvalue = "red")
+	// private String color;
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
-		return lastName;
-	}
+	// public String getLastName() {
+	// return lastName;
+	// }
+	//
+	// public void setEmail(String eMail) {
+	// this.email = eMail;
+	// }
+	//
+	// public String getEmail() {
+	// return email;
+	// }
 
-	public void setEmail(String eMail) {
-		this.email = eMail;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-	
 	public void setGitHub(String github) {
 		this.gitHub = github;
 	}
@@ -88,7 +86,7 @@ public class DemoForm implements Serializable {
 	public String getGitHub() {
 		return gitHub;
 	}
-	
+
 	public void setWebSite(String website) {
 		this.webSite = website;
 	}
@@ -97,17 +95,20 @@ public class DemoForm implements Serializable {
 		return webSite;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public String getFirstName() {
+		return firstName;
 	}
-
-	public Integer getNumber() {
-		return number;
-	}
-
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
+	// public void setLastName(String lastName) {
+	// this.lastName = lastName;
+	// }
+	//
+	// public Integer getNumber() {
+	// return number;
+	// }
+	//
+	// public void setNumber(Integer number) {
+	// this.number = number;
+	// }
 
 	public String getPassword() {
 		return password;
@@ -117,33 +118,33 @@ public class DemoForm implements Serializable {
 		this.password = password;
 	}
 
-	public String getGender() {
-		return gender;
-	}
+	// public String getGender() {
+	// return gender;
+	// }
+	//
+	// public String getAddress() {
+	// return address;
+	// }
+	//
+	// public void setAddress(String address) {
+	// this.address = address;
+	// }
+	//
+	// public String getCivilState() {
+	// return civilState;
+	// }
+	//
+	// public void setCivilState(String civilState) {
+	// this.civilState = civilState;
+	// }
+	//
+	// public String getCurrency() {
+	// return currency;
+	// }
 
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCivilState() {
-		return civilState;
-	}
-
-	public void setCivilState(String civilState) {
-		this.civilState = civilState;
-	}
-
-	public String getCurrency() {
-		return currency;
-	}
-
-	public String getColor() {
-		return color;
-	}
+	// public String getColor() {
+	// return color;
+	// }
 
 	private static final long serialVersionUID = -5073515619469444978L;
 }
---
+++ src/main/java/io/asfjava/ui/core/form/Index.java
@@ -0,0 +1,14 @@
+package io.asfjava.ui.core.form;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Index {
+
+	int value() default Integer.MAX_VALUE;
+}
