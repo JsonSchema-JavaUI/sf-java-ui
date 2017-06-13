ecc0065e4893189ec3bec15992df03a2cd18bf3c
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-06-13 21:42:57 +0100

    :art: A "NullPointerException" could be thrown;
    fix #54 sonar violation

--- src/main/java/io/asfjava/ui/core/schema/decorators/PasswordSchemaDecorator.java
+++ src/main/java/io/asfjava/ui/core/schema/decorators/PasswordSchemaDecorator.java
@@ -1,5 +1,7 @@
 package io.asfjava.ui.core.schema.decorators;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.types.StringSchema;
@@ -10,19 +12,20 @@ public class PasswordSchemaDecorator implements SchemaDecorator {
 
 	@Override
 	public void customizeSchema(BeanProperty property, JsonSchema jsonschema) {
-		Password annotation = property.getAnnotation(Password.class);
-		if (annotation != null && annotation.title() != null) {
-			((StringSchema) jsonschema).setTitle(annotation.title());
-		}
-		if (annotation.pattern() != null) {
-			((StringSchema) jsonschema).setPattern(annotation.pattern());
-		}
-		if (annotation.minLenght() != 0) {
-			((StringSchema) jsonschema).setMinLength(annotation.minLenght());
-		}
-		if (annotation.maxLenght() != Integer.MAX_VALUE) {
-			((StringSchema) jsonschema).setMaxLength(annotation.maxLenght());
-		}
+		Optional.ofNullable(property.getAnnotation(Password.class)).ifPresent(annotation -> {
+			if (annotation.title() != null) {
+				((StringSchema) jsonschema).setTitle(annotation.title());
+			}
+			if (annotation.pattern() != null) {
+				((StringSchema) jsonschema).setPattern(annotation.pattern());
+			}
+			if (annotation.minLenght() != 0) {
+				((StringSchema) jsonschema).setMinLength(annotation.minLenght());
+			}
+			if (annotation.maxLenght() != Integer.MAX_VALUE) {
+				((StringSchema) jsonschema).setMaxLength(annotation.maxLenght());
+			}
+		});
 	}
 
 	@Override
--- src/main/java/io/asfjava/ui/core/schema/decorators/TextAreaSchemaDecorator.java
+++ src/main/java/io/asfjava/ui/core/schema/decorators/TextAreaSchemaDecorator.java
@@ -1,5 +1,7 @@
 package io.asfjava.ui.core.schema.decorators;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.types.StringSchema;
@@ -10,17 +12,17 @@ public class TextAreaSchemaDecorator implements SchemaDecorator {
 
 	@Override
 	public void customizeSchema(BeanProperty property, JsonSchema jsonschema) {
-		TextArea annotation = property.getAnnotation(TextArea.class);
-		if (annotation != null && annotation.title() != null) {
-			((StringSchema) jsonschema).setTitle(annotation.title());
-		}
-		
-		if (annotation.minLenght() != 0) {
-			((StringSchema) jsonschema).setMinLength(annotation.minLenght());
-		}
-		if (annotation.maxLenght() != Integer.MAX_VALUE) {
-			((StringSchema) jsonschema).setMaxLength(annotation.maxLenght());
-		}
+		Optional.ofNullable(property.getAnnotation(TextArea.class)).ifPresent(annotation -> {
+			if (annotation.title() != null) {
+				((StringSchema) jsonschema).setTitle(annotation.title());
+			}
+			if (annotation.minLenght() != 0) {
+				((StringSchema) jsonschema).setMinLength(annotation.minLenght());
+			}
+			if (annotation.maxLenght() != Integer.MAX_VALUE) {
+				((StringSchema) jsonschema).setMaxLength(annotation.maxLenght());
+			}
+		});
 	}
 
 	@Override
