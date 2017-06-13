5cb2e30b9c102fc3fb19f13739adf9cc5927ba38
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-06-13 21:48:07 +0100

    remove unused block
    [ci skip]

--- src/main/java/io/asfjava/ui/core/schema/UiFormSchemaGenerator.java
+++ src/main/java/io/asfjava/ui/core/schema/UiFormSchemaGenerator.java
@@ -163,43 +163,4 @@ public final class UiFormSchemaGenerator {
 
 	private UiFormSchemaGenerator() {
 	}
-
-	public static void main(String[] argv) {
-
-		Map<String, Integer> unsortMap = new HashMap<>();
-		unsortMap.put("z", 10);
-		unsortMap.put("b", 5);
-		unsortMap.put("a", 6);
-		unsortMap.put("c", 20);
-		unsortMap.put("d", 1);
-		unsortMap.put("e", 7);
-		unsortMap.put("y", 8);
-		unsortMap.put("n", 99);
-		unsortMap.put("g", 50);
-		unsortMap.put("m", 2);
-		unsortMap.put("f", 9);
-
-		System.out.println("Original...");
-		System.out.println(unsortMap);
-
-		// sort by keys, a,b,c..., and return a new LinkedHashMap
-		// toMap() will returns HashMap by default, we need LinkedHashMap to
-		// keep the order.
-		Map<String, Integer> result = unsortMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
-				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue,
-						LinkedHashMap::new));
-
-		// Not Recommend, but it works.
-		// Alternative way to sort a Map by keys, and put it into the "result"
-		// map
-		Map<String, Integer> result2 = new LinkedHashMap<>();
-		unsortMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
-				.forEachOrdered(x -> result2.put(x.getKey(), x.getValue()));
-
-		System.out.println("Sorted...");
-		System.out.println(result);
-		System.out.println(result2);
-
-	}
-
 }
