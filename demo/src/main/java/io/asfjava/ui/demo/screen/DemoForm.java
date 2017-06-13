dee0666d59c1e892ca39383b19e5a5969762b4da
Saif Jerbi <m.jerbi.saif@gmail.com>
2017-06-13 21:28:36 +0100

    :green_heart: NPE when adding fields without @index
    fix #53

--- demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
+++ demo/src/main/java/io/asfjava/ui/demo/screen/DemoForm.java
@@ -2,148 +2,55 @@ package io.asfjava.ui.demo.screen;
 
 import java.io.Serializable;
 
-import io.asfjava.ui.core.form.CheckBox;
-import io.asfjava.ui.core.form.ComboBox;
-import io.asfjava.ui.core.form.Number;
-import io.asfjava.ui.core.form.Password;
-import io.asfjava.ui.core.form.RadioBox;
+import io.asfjava.ui.core.form.Index;
 import io.asfjava.ui.core.form.Tab;
-import io.asfjava.ui.core.form.TextArea;
 import io.asfjava.ui.core.form.TextField;
 
 public class DemoForm implements Serializable {
 
-	
-	@TextField(title = "Pesonal Website",fieldAddonLeft="http://", description = "This is TextField with fieldAddonLeft")
-	private String webSite;
-	
-	@TextField(title = "Your Github Mail",fieldAddonRight="@github.com", description = "This is TextField with fieldAddonRight")
-	private String gitHub;
-	
-//	@Tab(title = "Contact", index = 2)
-	@Password(title = "Password", placeHolder = "Please set you password", description = "This is password")
-	private String password;
+	@Index(1)
+	@TextField(title = "index 1")
+	private String index1;
 
-	@Tab(title = "Info", index = 1)
-	@TextField(title = "First Name", placeHolder = "Your first name", description = "This is a description for your first name field")
-	private String firstName;
+	@Tab(index = 2, title = "Tab2")
+	@Index(0)
+	@TextField(title = "index 2")
+	private String index2;
 
-//	@Tab(title = "Info", index = 1)
-	@TextField(title = "Last Name", placeHolder = "Your last name")
-	private String lastName;
+	@Tab(index = 3, title = "Tab1")
+	@Index(2)
+	@TextField(title = "Tab1 f3 index 2")
+	private String index3;
 
-	@Tab(title = "Contact", index = 2)
-	@TextField(title = "eMail", placeHolder = "Your email", pattern = "^\\S+@\\S+$", validationMessage = "Your mail must be in this format jhondoe@example.com", description = "This is Text Field with pattern and validation message")
-	private String email;
+	@Tab(index = 3, title = "Tab1")
+	@Index(1)
+	@TextField(title = "Tab1 f4 index 1")
+	private String index4;
 
-	@Tab(title = "Additional Info", index = 3)
-	@Number(title = "Number of children", placeHolder = "Number of children", description = "This is a number")
-	private Integer number;
-
-
-	@Tab(title = "Info", index = 1)
-	@ComboBox(title = "Gender", titleMap = GenderTitleMap.class)
-	private String gender;
-
-//	@Tab(title = "Additional Info", index = 3)
-	@ComboBox(title = "Currency", values = { "euro", "dollar" })
-	private String currency;
-
-	@Tab(title = "Additional Info", index = 3)
-	@RadioBox(title = "Civil State", titleMap = CivilStateTitelsMap.class)
-	private String civilState;
-
-//	@Tab(title = "Contact", index = 2)
-	@TextArea(title = "Address", placeHolder = "Fill your address please", description = "This is textarea")
-	private String address;
-
-	@Tab(title = "Additional Info", index = 3)
-	@CheckBox(title = "Color", values = { "red", "bleu", "green" }, defaultvalue = "red")
-	private String color;
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setEmail(String eMail) {
-		this.email = eMail;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setGitHub(String github) {
-		this.gitHub = github;
-	}
-
-	public String getGitHub() {
-		return gitHub;
-	}
-	
-	public void setWebSite(String website) {
-		this.webSite = website;
-	}
-
-	public String getWebSite() {
-		return webSite;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Integer getNumber() {
-		return number;
-	}
-
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCivilState() {
-		return civilState;
-	}
-
-	public void setCivilState(String civilState) {
-		this.civilState = civilState;
-	}
-
-	public String getCurrency() {
-		return currency;
-	}
-
-	public String getColor() {
-		return color;
-	}
+	@Tab(index = 2, title = "Tab2")
+	@TextField(title = "Tab2 f5 index 0")
+	private String index5;
 
 	private static final long serialVersionUID = -5073515619469444978L;
+
+	public String getIndex1() {
+		return index1;
+	}
+
+	public String getIndex2() {
+		return index2;
+	}
+
+	public String getIndex4() {
+		return index4;
+	}
+
+	public String getIndex5() {
+		return index5;
+	}
+
+	public String getIndex3() {
+		return index3;
+	}
+
 }
--- src/main/java/io/asfjava/ui/core/schema/UiFormSchemaGenerator.java
+++ src/main/java/io/asfjava/ui/core/schema/UiFormSchemaGenerator.java
@@ -10,7 +10,9 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -35,17 +37,36 @@ public final class UiFormSchemaGenerator {
 
 		JsonSchemaGenerator schemaGen = initSchemaGen(mapper);
 		JsonSchema schema = generateSchema(formDto, schemaGen);
+
 		Map<Field, JsonNode> nodes = initFieldFormDefinition(mapper, declaredFields);
+
+		Map<Field, JsonNode> sortedNodes = reorderFieldsBasedOnIndex(nodes);
 
 		handlerGroupedFields();
 
-		ObjectNode tabbedFields = handleTabbedFields(mapper, declaredFields, nodes);
+		ObjectNode tabbedFields = handleTabbedFields(mapper, declaredFields, sortedNodes);
 
 		ArrayNode formDefinition = mapper.createArrayNode();
 		formDefinition.add(tabbedFields);
-		nodes.entrySet().stream().forEach(nodesElement -> formDefinition.add(nodesElement.getValue()));
+		sortedNodes.entrySet().stream().forEach(nodesElement -> formDefinition.add(nodesElement.getValue()));
 
 		return new UiForm(schema, formDefinition);
+	}
+
+	private Map<Field, JsonNode> reorderFieldsBasedOnIndex(Map<Field, JsonNode> nodes) {
+
+		Comparator<? super Entry<Field, JsonNode>> tabIndexComparator = (entry1, entry2) -> {
+
+			Index field1Index = entry1.getKey().getAnnotation(Index.class);
+			Index field2Index = entry2.getKey().getAnnotation(Index.class);
+
+			return Integer.compare((field1Index != null ? field1Index.value() : Integer.MAX_VALUE),
+					field2Index != null ? field2Index.value() : Integer.MAX_VALUE);
+		};
+
+		return nodes.entrySet().stream().sorted(tabIndexComparator).collect(Collectors.toMap(Map.Entry::getKey,
+				Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
 	}
 
 	private void handlerGroupedFields() {
@@ -55,12 +76,21 @@ public final class UiFormSchemaGenerator {
 
 	private ObjectNode handleTabbedFields(ObjectMapper mapper, Field[] declaredFields, Map<Field, JsonNode> nodes) {
 		Predicate<? super Field> checkTabAnnotation = field -> field.isAnnotationPresent(Tab.class);
+
 		Comparator<? super Field> tabIndexComparator = (field1, field2) -> Integer
 				.compare(field1.getAnnotation(Tab.class).index(), field2.getAnnotation(Tab.class).index());
 
+		Comparator<? super Field> fieldIndexComparator = (entry1, entry2) -> {
+			Index field1Index = entry1.getAnnotation(Index.class);
+			Index field2Index = entry2.getAnnotation(Index.class);
+
+			return Integer.compare((field1Index != null ? field1Index.value() : Integer.MAX_VALUE),
+					field2Index != null ? field2Index.value() : Integer.MAX_VALUE);
+		};
+
 		Map<String, List<JsonNode>> groupedFieldsByTab = new LinkedHashMap<>();
 
-		Arrays.stream(declaredFields).filter(checkTabAnnotation).sorted(tabIndexComparator)
+		Arrays.stream(declaredFields).filter(checkTabAnnotation).sorted(fieldIndexComparator).sorted(tabIndexComparator)
 				.forEach(field -> groupFieldsByTab(nodes, field, groupedFieldsByTab));
 
 		ArrayNode tabs = mapper.createArrayNode();
@@ -82,15 +112,10 @@ public final class UiFormSchemaGenerator {
 	}
 
 	private Map<Field, JsonNode> initFieldFormDefinition(ObjectMapper mapper, Field[] declaredFields) {
-		Map<Field, JsonNode> nodes = new LinkedHashMap<>();
-		
-		Predicate<? super Field> checkIndexAnnotation = field -> field.isAnnotationPresent(Index.class);
-		Comparator<? super Field> fieldIndexComparator = (field1, field2) -> Integer
-				.compare(field1.getAnnotation(Index.class).value(), field2.getAnnotation(Index.class).value());
+		Map<Field, JsonNode> nodes = new HashMap<>();
 
-		Arrays.stream(declaredFields).filter(checkIndexAnnotation).sorted(fieldIndexComparator)
-				.forEachOrdered(field -> buildFormDefinition(nodes, mapper, field));
-		
+		Arrays.stream(declaredFields).forEach(field -> buildFormDefinition(nodes, mapper, field));
+
 		return nodes;
 	}
 
@@ -137,6 +162,44 @@ public final class UiFormSchemaGenerator {
 	}
 
 	private UiFormSchemaGenerator() {
+	}
+
+	public static void main(String[] argv) {
+
+		Map<String, Integer> unsortMap = new HashMap<>();
+		unsortMap.put("z", 10);
+		unsortMap.put("b", 5);
+		unsortMap.put("a", 6);
+		unsortMap.put("c", 20);
+		unsortMap.put("d", 1);
+		unsortMap.put("e", 7);
+		unsortMap.put("y", 8);
+		unsortMap.put("n", 99);
+		unsortMap.put("g", 50);
+		unsortMap.put("m", 2);
+		unsortMap.put("f", 9);
+
+		System.out.println("Original...");
+		System.out.println(unsortMap);
+
+		// sort by keys, a,b,c..., and return a new LinkedHashMap
+		// toMap() will returns HashMap by default, we need LinkedHashMap to
+		// keep the order.
+		Map<String, Integer> result = unsortMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue,
+						LinkedHashMap::new));
+
+		// Not Recommend, but it works.
+		// Alternative way to sort a Map by keys, and put it into the "result"
+		// map
+		Map<String, Integer> result2 = new LinkedHashMap<>();
+		unsortMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+				.forEachOrdered(x -> result2.put(x.getKey(), x.getValue()));
+
+		System.out.println("Sorted...");
+		System.out.println(result);
+		System.out.println(result2);
+
 	}
 
 }
